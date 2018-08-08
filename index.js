741ec3c65e6d2fc04253f2b60b732db448f91652
Son Truong <sntruong12@gmail.com>
2018-08-07 20:19:29 -0500

    added public directory and created express server to display static files

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ console.log("I'm running now!");
 
 var Twit = require('twit');
 var config = require('./config.js');
+var express = require('express');
+
+var server = express();
+server.use(express.static('public'))
+
+var port = 1337;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
+
+
 
 var T = new Twit(config);
 
-//  look into making this block of code it's own module
-//  search twitter for all tweets containing specified words
+//
+//  search twitter for 5 recent tweets containing Anthony Bourdain
 //
 
 var params = {
-  q: '#AnthonyBourdain',
+  q: 'Anthony Bourdain',
   count: 5
 };
 
@@ -20,7 +31,7 @@ function gotTweets(err, data, response) {
   var tweets = data.statuses;
 
   for (var i = 0; i < tweets.length; i++) {
-    console.log(`Tweet #${i+1}: ${tweets[i].text} -@${tweets[i].user.screen_name}`);
+    console.log(`Tweet #${i+1}: ${tweets[i].text} tweeted by @${tweets[i].user.screen_name}`);
   }
 
 };
