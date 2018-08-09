bb2ae45898e6a52c0d862d3d8f4d1b494b13f907
Son Truong <sntruong12@gmail.com>
2018-08-08 22:21:09 -0500

    Added the query for tweets containing images from AB's twitter account

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("I'm running now!");
 
 var Twit = require('twit');
+
 var config = require('./config.js');
 var express = require('express');
 
@@ -17,21 +18,53 @@ server.listen(port, function() {
 var T = new Twit(config);
 
 //
-//  search twitter for 5 recent tweets containing Anthony Bourdain
+//  search twitter for 5 recent tweets
 //
 
-var params = {
-  q: 'Anthony Bourdain',
-  count: 5
+// var tweetParams = {
+//   q: 'Anthony Bourdain',
+//   count: 5
+// };
+//
+// T.get('search/tweets', tweetParams, gotTweets);
+//
+// function gotTweets(err, data, response) {
+//   var tweets = data.statuses;
+//
+//   for (var i = 0; i < tweets.length; i++) {
+//     console.log(`Tweet #${i+1}: ${tweets[i].text} tweeted by @${tweets[i].user.screen_name}`);
+//   }
+//
+// };
+
+//
+// get media
+//
+
+var mediaParams = {
+  screen_name: 'Bourdain',
+  count: 100,
+  exclude_replies: true,
+  include_rts: false
 };
 
-T.get('search/tweets', params, gotTweets);
+var mediaUrlArray = [];
 
-function gotTweets(err, data, response) {
-  var tweets = data.statuses;
+T.get('statuses/user_timeline', mediaParams, gotMedia);
 
-  for (var i = 0; i < tweets.length; i++) {
-    console.log(`Tweet #${i+1}: ${tweets[i].text} tweeted by @${tweets[i].user.screen_name}`);
+function gotMedia(err, data, response) {
+  // mediaUrl = data[0].entities.media[0].media_url_https;
+  // console.log(mediaUrl);
+  var returnedTweets = data.length
+  console.log(returnedTweets + " tweets in this response");
+
+  for (var i = 0; i < returnedTweets; i++) {
+    if (data[i].entities.hasOwnProperty('media')) {
+      console.log(`Tweet #${i+1} has an image located at this link - ${data[i].entities.media[0].media_url_https}`);
+      mediaUrlArray.push(data[i].entities.media[0].media_url_https);
+    }
   }
+
+  console.log(mediaUrlArray);
 
 };
